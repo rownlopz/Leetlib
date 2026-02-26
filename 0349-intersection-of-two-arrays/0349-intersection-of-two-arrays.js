09331a95d97855791ececdc863aa087dba115d2e
rownlopz <145683644+rownlopz@users.noreply.github.com>
2026-02-26 14:54:13 +0530

    Time: 2 ms (41.31%), Space: 54.1 MB (86.34%) - LeetHub

---
+++ 0349-intersection-of-two-arrays/0349-intersection-of-two-arrays.js
@@ -0,0 +1,21 @@
+/**
+ * @param {number[]} nums1
+ * @param {number[]} nums2
+ * @return {number[]}
+ */
+var intersection = function(nums1, nums2) {
+    var result = [];
+    for(let i = 0 ; i < nums1.length ; i++){
+        for(let j = 0 ; j < nums2.length ; j++){
+            if(nums1[i] === nums2[j]){
+                if(result.includes(nums1[i])){
+                    break;
+                }else{
+                    result.push(nums1[i]);
+                    break; 
+                }            
+            }
+        }
+    }
+    return result;
+};

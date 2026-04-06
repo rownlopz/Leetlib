ef003db572957332a112a09fbc7a80be59f8b13f
rownlopz <145683644+rownlopz@users.noreply.github.com>
2026-04-06 11:43:42 +0530

    Time: 5 ms (2%), Space: 54.9 MB (89.2%) - LeetHub

---
+++ 3190-find-minimum-operations-to-make-all-elements-divisible-by-three/3190-find-minimum-operations-to-make-all-elements-divisible-by-three.js
@@ -0,0 +1,16 @@
+/**
+ * @param {number[]} nums
+ * @return {number}
+ */
+var minimumOperations = function(nums) {
+    let op = 0;
+    for(let i = 0; i<nums.length;i++){
+        if(nums[i] === 1){
+            op++;
+        }
+        else if([nums[i]+1]%3 === 0 || [nums[i]-1]%3 === 0){
+            op++;
+        }
+    }
+    return op;
+};

352913c3ff26f97458e4355afda021dfe4328843
rownlopz <145683644+rownlopz@users.noreply.github.com>
2026-04-19 15:14:30 +0530

    Time: 0 ms (100%), Space: 57.3 MB (19.17%) - LeetHub

---
+++ 2824-count-pairs-whose-sum-is-less-than-target/2824-count-pairs-whose-sum-is-less-than-target.js
@@ -0,0 +1,17 @@
+/**
+ * @param {number[]} nums
+ * @param {number} target
+ * @return {number}
+ */
+var countPairs = function(nums, target) {
+    let n = nums.length;
+    let count = 0;
+    for(let i = 0; i < n; i++){
+        for(let j = i+1; j < n; j++){
+            if(nums[i]+nums[j] < target){
+                count++;
+            }
+        }
+    }
+    return count;
+};

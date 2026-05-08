b11370e2e0aaef3381abe96649311a778f96e83c
rownlopz <145683644+rownlopz@users.noreply.github.com>
2026-05-08 10:29:09 +0530

    Time: 2 ms (40.2%), Space: 57.2 MB (13.72%) - LeetHub

--- 2652-sum-multiples/2652-sum-multiples.js
+++ 2652-sum-multiples/2652-sum-multiples.js
@@ -4,7 +4,7 @@
  */
 var sumOfMultiples = function(n) {
     let sum = 0
-    for(i=1;i<=n;i++){
+    for(i=0;i<=n;i++){
         if(i%3 === 0 || i%5 === 0 || i%7 === 0){
             sum+=i;
         }

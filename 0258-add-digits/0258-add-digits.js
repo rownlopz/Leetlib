c5ef54dd35dd91f579ec8faa0905bcc860504d78
rownlopz <145683644+rownlopz@users.noreply.github.com>
2026-09-19 19:08:10 +0530

    Time: 0 ms (100%), Space: 55.5 MB (81.56%) - LeetHub

--- 0258-add-digits/0258-add-digits.js
+++ 0258-add-digits/0258-add-digits.js
@@ -2,11 +2,18 @@
  * @param {number} num
  * @return {number}
  */
+// var addDigits = function(num) {
+//     while(num>=10){
+//         num = String(num)
+//         .split('')
+//         .reduce((acc,cur) => acc + Number(cur),0);
+//     }
+//     return num;
+// }
+
 var addDigits = function(num) {
-    while(num>=10){
-        num = String(num)
-        .split('')
-        .reduce((acc,cur) => acc + Number(cur),0);
+    while(num>=10) {
+        num = String(num).split('').reduce((acc,cur) => acc + Number(cur),0);
     }
     return num;
 }
